688d520492fe94b37df6b5f8edc657e59270b2b1
agungdp22 <agungdwiprasetyo22@gmail.com>
2017-03-25 23:37:01 +0700

    first init

---
+++ pythonclient/client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from socketIO_client import SocketIO, LoggingNamespace
+
+import numpy as np
+from random import uniform
+
+socketIO = SocketIO("http://localhost", 5000, LoggingNamespace)
+
+def getData(*args):
+	print(args[0])
+
+if __name__ == '__main__':
+	print ("Listening server...")
+	socketIO.on('chat message', getData)
+	socketIO.wait()
